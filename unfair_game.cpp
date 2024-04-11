de748cfd8e834b6d0996f57807cc41c5bffbfa66
pengmingyi <1601690791@qq.com>
2024-04-11 23:54:39 +0800

    Codeforces Round 938 (Div. 3) F

---
+++ unfair_game.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+	int N = 0;
+	int a, b, c, d;
+	cin >> N;
+	for(int i = 0; i < N; ++i){
+		int temp = 0;
+		cin >> a >> b >> c >> d;
+		temp += (a/2 + b/2 + c/2 + d/2);
+		if( a %2 == 1 && b % 2 == 1 && c % 2 == 1)
+			temp += 1;
+		cout << temp << endl;
+	}
+	return 0;
+} 
